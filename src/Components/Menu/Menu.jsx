50aa2b7f57b07cd225a5619e4a53005053c3e7ce
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-09 11:57:24 +0300

    fixed style

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,24 +5,63 @@ import s from './Menu.module.scss';
 export default function Menu() {
     const [isActive, setIsActive] = useState(false);
 
+    const redirectLink = (link) => {
+        window.location.replace(link);
+    }
+
+    const links = [
+        {
+            image: './image/web.svg',
+            link: '###',
+            name: 'web'
+        },
+        {
+            image: './image/contact.svg',
+            link: '###',
+            name: 'contact'
+        },
+        {
+            image: './image/share.svg',
+            link: '###',
+            name: 'share'
+        }
+    ]
     return (
         <div className={s.menu_wrapper}>
-            <div className={isActive ? s.burger + ' ' + s.active : s.burger} onClick={() => setIsActive(!isActive)}>
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className={s.menu}>
+                <div className={isActive ? s.burger + ' ' + s.active : s.burger} onClick={() => setIsActive(!isActive)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className={s.buttons} style={{ opacity: 0, position: 'relative', pointerEvents: 'none' }}>
+                    <button></button>
+                    {links.map((link, index) => {
+                        return (
+                            <button
+                                key={index}
+                                onClick={() => redirectLink(link.link)}
+                            >
+                                <img src={link.image} alt={link.name} />
+                            </button>
+                        )
+                    })}
+                </div>
+                <div className={s.buttons} style={{ transform: isActive ? 'translateY(0%)' : `translateY(-150%)`, opacity: isActive ? `1` : '0' }}>
+                    <button></button>
+                    {links.map((link, index) => {
+                        return (
+                            <button
+                                key={index}
+                                onClick={() => redirectLink(link.link)}
+                                style={{ opacity: isActive ? `1` : '0', transitionDelay: isActive ? `100ms` : '0', transitionDuration: isActive ? `0` : '300ms' }}
+                            >
+                                <img src={link.image} alt={link.name} />
+                            </button>
+                        )
+                    })}
+                </div>
             </div>
-            <div className={!isActive ? s.buttons + ' ' + s.active : s.buttons}>
-                <a href="###">
-                    <img src="./image/web.svg" alt="" />
-                </a>
-                <a href="###">
-                    <img src="./image/contact.svg" alt="" />
-                </a>
-                <a href="###">
-                    <img src="./image/share.svg" alt="" />
-                </a>
-            </div>
-        </div>
+        </div >
     )
 } 
