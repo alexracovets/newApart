19f08bf7562da17ea4cd677c190a3d803f6a8c92
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-06-27 11:13:52 +0300

    fixed style

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store/store';
 
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
